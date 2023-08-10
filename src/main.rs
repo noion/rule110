36efb17baf8d14b3f3e1c416a5634c7d82298b0a
m.davydov <m.davydov@tinkoff.ru>
2023-08-10 23:22:01 +0300

    Rule 110 implementation

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+const BOARD_SIZE: usize = 100;
+
+fn main() {
+    let mask = [0, 1, 1, 1, 0, 1, 1, 0];
+
+    let mut current_board = 1;
+    let mut board = [[0; BOARD_SIZE]; 2];
+    let sid = BOARD_SIZE - 2;
+
+    board[current_board][sid] = 1;
+
+    for _i in 1..sid {
+        for j in 0..BOARD_SIZE {
+            if board[current_board][j] == 0 {
+                print!(" ")
+            } else {
+                print!("*");
+            }
+        }
+        let next_board = 1 - current_board;
+        for i in 1..BOARD_SIZE - 1 {
+            let a = board[current_board][i - 1];
+            let b = board[current_board][i];
+            let c = board[current_board][i + 1];
+            let pattern = (a << 2) | (b << 1) | (c << 0);
+            board[next_board][i] = mask[pattern];
+        }
+        current_board = next_board;
+        println!()
+    }
+}
